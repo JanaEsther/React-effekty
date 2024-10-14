3897189b8b03cf1258b63fddf66573464aa02ff2
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-10-14 15:27:33 +0200

    ukol-1

--- src/components/Ukol1/index.jsx
+++ src/components/Ukol1/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { useState } from 'react';
 /*
 Zadání: Upravte efekt v této komponentě tak, aby se při KAŽDÉM vykreslení komponenty v konzoli objevil
   text „Jsem tady“.
@@ -9,13 +10,17 @@ Poznámka: Nemáte jak prakticky ověřit, že se to zobrazí při každém vykr
 */
 
 export const Ukol1 = () => {
+  const [text, setText] = useState('');
+
+  const message = 'Jsem tady.';
+
   useEffect(() => {
     console.log('Jsem tady.');
   });
 
   return (
     <>
-      <p>Jsem tady.</p>
+      <p>{message}</p>
     </>
   );
 };
